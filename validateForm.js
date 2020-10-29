5ff22b026783ae4909f9696426105e6b74f261eb
mobronfin <59948664+mobronfin@users.noreply.github.com>
2020-10-29 18:22:50 -0400

    fourth take

--- validateForm.js
+++ validateForm.js
@@ -1,35 +1,29 @@
 function validateContactForm() {
     var firstname =  document.getElementById('firstname').value;
     if (firstname == "") {
-        document.querySelector('.status').innerHTML = "First name cannot be empty";
+        document.querySelector('.status').innerHTML = "Please put in first name";
         return false;
     }
 
     var lastname =  document.getElementById('lastname').value;
     if (lastname == "") {
-        document.querySelector('.status').innerHTML = "Last name cannot be empty";
+        document.querySelector('.status').innerHTML = "Please put in last name";
         return false;
     }
 
     var email =  document.getElementById('email').value;
     if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        document.querySelector('.status').innerHTML = "Please put in email";
         return false;
-    } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
-            return false;
-        }
     }
     var subject =  document.getElementById('subject').value;
     if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        document.querySelector('.status').innerHTML = "Please put in subject";
         return false;
     }
     var message =  document.getElementById('message').value;
     if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        document.querySelector('.status').innerHTML = "Please put in message";
         return false;
     }
     document.querySelector('.status').innerHTML = "Sent";
